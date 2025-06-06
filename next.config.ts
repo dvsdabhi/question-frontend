7204d1c90f148755abbe24ff3ea3339309bc90bd
dvsdabhi <divyeshd623@gmail.com>
2025-06-06 11:00:02 +0530

    remove unnecceracy import

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,6 @@ const nextConfig: NextConfig = {
     domains: ['127.0.0.1', 'divyeshdabhi.pythonanywhere.com'],
   },
   productionBrowserSourceMaps: true,
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    optimizeCss: true, // Optional: Vercel uses this behind the scenes
-  },
 };
 
 export default nextConfig;
